94002786c9a913be7382db9abe465a37b2ab94a6
TarasRogiv <r.taras.t.1@gmail.com>
2024-12-22 21:37:18 +0200

    final v.1

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
 const firstResButton = document.getElementById("first-task-res-button");
 const quantityField = document.getElementById("quantity");
 const pricePerDroidField = document.getElementById("pricePerDroid");
+const customerCredits = document.getElementById("customerCreditsCount");
 const resField = document.getElementById("first-task-result-field");
 
 firstResButton.addEventListener("click", () => {
   resField.value = makeTransaction(
     quantityField.value,
-    pricePerDroidField.value
+    pricePerDroidField.value,
+    customerCredits.value
   );
 });
 
+// Друге завдання
+
 const secondResButton = document.getElementById("second-task-res-button");
-const country = document.getElementById("country");
-const price = document.getElementById("price");
-const deliveryFee = document.getElementById("deliveryFee");
+const message = document.getElementById("country");
+const maxLength = document.getElementById("price");
 const resSecondField = document.getElementById("second-task-result-field");
 
 secondResButton.addEventListener("click", () => {
-  resSecondField.value = getShippingMessage(
-    country.value,
-    Number.parseInt(price.value),
-    Number.parseInt(deliveryFee.value)
-  );
+  resSecondField.value = formatMessage(message.value, maxLength.value);
 });
 
+// Третє завдання
+
 const thirdResButton = document.getElementById("third-task-res-button");
-const contentWidth = document.getElementById("content");
-const paddingWidth = document.getElementById("padding");
-const borderWidth = document.getElementById("border");
+const contentMessage = document.getElementById("third-tasc-message");
 const resThirdField = document.getElementById("third-task-result-field");
 
 thirdResButton.addEventListener("click", () => {
-  resThirdField.value = getElementWidth(
-    contentWidth.value,
-    paddingWidth.value,
-    borderWidth.value
-  );
+  resThirdField.value = checkForSpam(contentMessage.value);
+});
+
+// Четверте завдання
+
+const getCountry = document.getElementById("four-input-label");
+const fourResButton = document.getElementById("four-task-res-button");
+const resFourField = document.getElementById("four-task-result-field");
+
+fourResButton.addEventListener("click", () => {
+  resFourField.value = getShippingCost(getCountry.value);
 });
